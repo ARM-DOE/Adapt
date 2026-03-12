987879c7edb1d8d9751ca43cfb3f36a3af3abce5
Bhupendra Raut <braut@anl.gov>
2026-03-12 09:03:33 -0500

    all remaining init files added

---
+++ src/adapt/__init__.py
@@ -0,0 +1,11 @@
+"""``Adapt`` - Automated Detection And Projection of storm cells using Tracking.
+
+Subpackages:
+- radar: Data loading, segmentation, analysis
+- pipeline: Orchestrator, processor, tracking
+- visualization: Plotting
+
+Authors: Bhupendra Raut and Sid Gupta
+"""
+
+__version__ = "0.1.0"

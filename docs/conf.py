63e2ff71d0a57c176567781a722be4f8eb008116
Bhupendra Raut <braut@anl.gov>
2026-04-26 14:49:16 -0500

    FIX: Argonne Copyright added to Docs pages

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 
 project = "Adapt"
 author = "Bhupendra Raut"
-copyright = "2026, Bhupendra Raut"
+copyright = "2026, UChicago Argonne, LLC"
 release = "0.2.0"
 
 extensions = [
